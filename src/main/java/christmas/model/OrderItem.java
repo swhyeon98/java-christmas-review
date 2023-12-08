3cfd7635bf38e018ca65a3684901bd47e0aef334
swhyeon98 <smreo75@gmail.com>
2023-12-08 16:42:10 +0900

    feat(OrderItem): 메뉴 항목과 수량을 관리하는 'OrderItem' 추가 - 수량에 대한 검증 로직 추가

---
+++ src/main/java/christmas/model/OrderItem.java
@@ -0,0 +1,29 @@
+package christmas.model;
+
+public class OrderItem {
+
+    private final String menuName;
+    private final int quantity;
+
+    public OrderItem(String menuName, int quantity) {
+        this.menuName = menuName;
+        this.quantity = quantity;
+    }
+
+    private void validateQuantity(int quantity) {
+        isQuantityOverLimit(quantity);
+        isQuantityUnderLimit(quantity);
+    }
+
+    private void isQuantityOverLimit(int quantity) {
+        if (quantity > 20) {
+            throw new IllegalArgumentException("[ERROR] 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private void isQuantityUnderLimit(int quantity) {
+        if (quantity < 1) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+}
